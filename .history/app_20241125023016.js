758b76fc5d9f9a36d27ec4b429714cdd778d8fac
Ryan Evans <rdevans87@gmail.com>
2024-11-25 02:32:07 -0500

    app,js setup local storage and elements

---
+++ .history/app_20241125023016.js
@@ -0,0 +1,3 @@
+// Local Storage Keys
+const LOCAL_STORAGE_SESSION = "planningPokerSession";
+const LOCAL_STORAGE_ESTIMATES = "planningPokerEstimates";
---
+++ .history/app_20241125023055.js
@@ -0,0 +1,4 @@
+// Local Storage Keys
+const LOCAL_STORAGE_SESSION = "planningPokerSession";
+const LOCAL_STORAGE_ESTIMATES = "planningPokerEstimates";
+

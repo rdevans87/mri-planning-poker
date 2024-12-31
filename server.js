015e6ba941120618fc65f3a84280dbd87f567fad
Ryan Evans <rdevans87@gmail.com>
2024-12-30 22:07:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
-import cors from 'cors';
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors'); // Import CORS
 
 const app = express();
-
 const server = http.createServer(app);
 const io = new Server(server);
 
